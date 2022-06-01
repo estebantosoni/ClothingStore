8e10aab4ecb6abf4db9bd03bc32dd7f61365f15b
estebantosoni <tosoniesteban@gmail.com>
2022-06-01 10:21:19 -0300

    Subiendo carpetas base del proyecto

---
+++ tienda_back/src/main/java/ar/store/italiana/tienda_back/tienda_back/TiendaBackApplication.java
@@ -0,0 +1,13 @@
+package ar.store.italiana.tienda_back.tienda_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBackApplication.class, args);
+	}
+
+}
---
+++ tienda_back/src/test/java/ar/store/italiana/tienda_back/tienda_back/TiendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.store.italiana.tienda_back.tienda_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
